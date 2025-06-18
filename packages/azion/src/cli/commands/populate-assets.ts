9206404df6899144d18bf3398900b166e1526771
José Filho <jc.bsfilho@gmail.com>
2025-06-18 11:21:21 -0300

    feat: add populateAssets command and update args type (#8)

---
+++ packages/azion/src/cli/commands/populate-assets.ts
@@ -0,0 +1,10 @@
+import path from "node:path";
+
+import { BuildOptions } from "@opennextjs/aws/build/helper.js";
+
+import { copyAssets } from "../../core/utils/copy-assets.js";
+
+export async function populateAssets(options: BuildOptions, previewOptions: { assetsDir: string }) {
+  // Copy static assets to the cache directory
+  copyAssets(path.join(options.outputDir, "assets"), path.join(previewOptions.assetsDir));
+}
